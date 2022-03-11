7e6bc3a2549c698fbc3fb5b7c41277b59595adc0
Juan Jose <juanjose.arcani2309@gmail.com>
2022-03-10 21:18:53 -0400

    feat: add form to register to modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import ReactModal from 'react-modal';
+import { useForm } from '../../hooks/useForm';
+import { AppContextInterface, AuthContext } from '../../services/contexts/AuthContext';
 import DashContext, { DashContextInterface } from '../../services/contexts/DashContext';
 import { types } from '../../services/types/types';
 import './modal.css'
-
 
 const customStyles = {
   content: {
@@ -16,26 +17,164 @@ const customStyles = {
   },
 };
 
+type Roles = 'ADMIN_ROL' | 'MANAGER_ROL' | 'TRAINER_ROL' | 'VENTAS_ROL';
+
+interface Employee {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+}
+
 const Modal = () => {
 
   const { dash: { toggle }, dispatch } = useContext(DashContext) as DashContextInterface;
+  const { user:userAuth } = useContext(AuthContext) as AppContextInterface;
+  const [ role, setRole ] = useState<Roles>('VENTAS_ROL');
+  let isAdmin = false;
+  if (userAuth.rol == 'ADMIN') {
+    isAdmin = true
+  }
+
+  const { handleSubmit, handleChange, data: user, errors } = useForm<Employee>({
+    validations: {
+      firstname: {
+        pattern: {
+          value: '^[a-zA-Z].*[\s\.]*$',
+          message: "Require un nombre",
+        },
+      },
+      lastname: {
+        pattern: {
+          value: '^[a-zA-Z].*[\s\.]*$',
+          message: 'Requiere un apellido',
+        },
+      },
+      email: {
+        pattern: {
+          value: '^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$',
+          message:
+            "Verifica que el correo nuevamente",
+        },
+      },
+      password: {
+        custom: {
+          isValid: (value) => value.length >= 6,
+          message: 'La contraseña necesita ser de al menos 6 characteres',
+        },
+      },
+    },
+    initialValues: {
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: '',
+    },
+    onSubmit: () => {
+      console.log({...user, rol: role})
+      // dispatch({ type: 'LOGIN', email: user.email, password: user.password })
+    },
+  });
 
   const closeModal = () => {
     dispatch({ type: types.dashCloseModal });
   }
 
+  const rolChange = (e:any) => {
+    setRole(e.target.value);
+  }
+
   return (
     <ReactModal
-      isOpen={ toggle }
-      onRequestClose={ closeModal }
-      closeTimeoutMS={ 100 }
+      isOpen={toggle}
+      onRequestClose={closeModal}
+      closeTimeoutMS={100}
       overlayClassName="app__modal-background"
-      style={ customStyles }
-      ariaHideApp={ false }
+      style={customStyles}
+      ariaHideApp={false}
     >
-      <form className="app__modal-form">
-        <h1>Añadir empleado o cliente :P</h1>
-        <button>Hello this is a modal</button>
+      <form
+        className="app__modal-form"
+        onSubmit={handleSubmit}
+      >
+        <h1>Añadir empleado</h1>
+        <div className="app__input-container">
+          <label className="app__input-label" htmlFor="firstname">Nombres</label>
+          <input
+            type="text"
+            id="firstname"
+            placeholder="Nombres"
+            autoComplete="off"
+            className="app__input input"
+            name="firstname"
+            value={ user.firstname }
+            onChange={handleChange('firstname')}
+          />
+          <i className="fa fa-envelope"></i>
+          {errors.firstname && <span className="app__input--error">{errors.firstname}</span>}
+        </div>
+        <div className="app__input-container">
+          <label className="app__input-label" htmlFor="lastname">Apellidos</label>
+          <input
+            type="text"
+            id="lastname"
+            placeholder="Apellidos"
+            autoComplete="off"
+            className="app__input input"
+            name="lasname"
+            value={user.lastname}
+            onChange={handleChange('lastname')}
+          />
+          <i className="fa fa-key"></i>
+          {errors.lastname && <span className="app__input--error">{errors.lastname}</span>}
+        </div>
+        <div className="app__input-container">
+          <label className="app__input-label" htmlFor="email">Correo</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Correo"
+            autoComplete="off"
+            className="app__input input"
+            name="email"
+            value={user.email}
+            onChange={handleChange('email')}
+          />
+          <i className="fa fa-key"></i>
+          {errors.email && <span className="app__input--error">{errors.email}</span>}
+        </div>
+        <div className="app__input-container">
+          <label className="app__input-label" htmlFor="password">Contraseña <span>(secret)</span></label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Contraseña"
+            autoComplete="off"
+            className="app__input input"
+            name="password"
+            value={user.password}
+            onChange={handleChange('password')}
+          />
+          <i className="fa fa-key"></i>
+          {errors.password && <span className="app__input--error">{errors.password}</span>}
+        </div>
+        <div className="app__input-container">
+          <label className="app__input-label" htmlFor="rol">Rol</label>
+          <select name="rol" id="rol" onChange={ rolChange } value={ role }>
+            { isAdmin && (<option value="ADMIN_ROL">Admin</option>)}
+            { isAdmin && (<option value="MANAGER_ROL">Manager</option>)}
+            <option value="TRAINER_ROL">Trainer</option>
+            <option value="VENTAS_ROL">Ventas</option>
+          </select>
+        </div>
+
+        <button
+          className="custom__button"
+          style={{ marginBottom: '.5rem' }}
+          type="submit"
+        >
+          CREAR
+        </button>
       </form>
     </ReactModal>
   )

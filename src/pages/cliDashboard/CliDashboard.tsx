a158048a735acfaf3c0e1c04ea055d63b89423ee
Juan Jose <juanjose.arcani2309@gmail.com>
2022-03-24 21:03:40 -0400

    feat: show cursos where the client signed in

--- src/pages/cliDashboard/CliDashboard.tsx
+++ src/pages/cliDashboard/CliDashboard.tsx
@@ -1,8 +1,81 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import Widget from '../../components/widget/Widget'
+import { AppContextInterface, AuthContext } from '../../services/contexts/AuthContext';
 
+import userImg from "../../assets/images/user.svg";
+import DashContext, { DashContextInterface } from '../../services/contexts/DashContext';
+import Loader from '../../components/loader/Loader';
 const CliDashboard = () => {
+
+  const { authDispatch, user } = useContext(AuthContext) as AppContextInterface;
+  const { dash, dashDispatch } = useContext(DashContext) as DashContextInterface;
+
+  useEffect(() => {
+    dashDispatch({ type: 'GETONE', endpoint: `clientes/${user.nombre}/cursos` });
+  }, [])
+
+  if (dash.checking == true || dash.activeData === undefined ) {
+    return (<Loader />)
+  }
+  const { cursos }:any = dash.activeData;
+  console.log(cursos)
   return (
-    <div>CliDashboard</div>
+    <div className="app__dashboard">
+      <h2 style={{ marginBottom: 15 }}>Dashboard</h2>
+      <div className="app__dashboard-row">
+        <div className="app__dashboard-item">
+          <Widget
+            title={`${user.nombre} ${user.apellido}`}
+          >
+            <div
+              className="app__widget-user"
+            >
+              <div className="app__widget-user_avatar">
+                <img src={ userImg } alt="user" />
+              </div>
+              <div className="app__widget-user-container">
+                <p className="app__widget-user_text">{user.email}</p>
+              </div>
+            </div>
+          </Widget>
+        </div>
+      </div>
+      <div className="app__dashboard-row">
+        <div className="app__dashboard-item">
+          <Widget
+            title="Mis cursos"
+          >
+            <div className="app__dashboard-row">
+              {
+                cursos.map((curso:any) => (
+                  <div key={curso.id}>
+                    <p>Título: <span>{curso.nombreCurso}</span></p>
+                    <p>Fecha de inicio: <span>{curso.fechaIni}</span></p>
+                    <p>Fecha fin: <span>{curso.fechaFin}</span></p>
+                    <p>
+                      {
+                        curso.cursoIniciado
+                        ? <span>Iniciado</span>
+                        : <span>No Iniciado</span>
+                      }
+                    </p>
+                  </div>
+                ))
+              }
+            </div>
+            <p>Tienes <span>{ cursos.length } cursos</span></p>
+          </Widget>
+        </div>
+        <div className="app__dashboard-item">
+          <Widget
+            title="Mi actividad"
+          >
+            Char about my daily acctivity
+          </Widget>
+        </div>
+      </div>
+      
+    </div>
   )
 }
 

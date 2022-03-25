c26bc59c4bdb060dd19ee1fa2a22ba8a28883e95
Juan Jose <juanjose.arcani2309@gmail.com>
2022-03-24 21:04:44 -0400

    feat: show available courses for the client

--- src/pages/cliCourses/CliCourses.tsx
+++ src/pages/cliCourses/CliCourses.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import Loader from '../../components/loader/Loader';
 import Widget from '../../components/widget/Widget';
+import DashContext, { DashContextInterface } from '../../services/contexts/DashContext';
 
 const CliCourses = () => {
+  const { dash, dashDispatch } = useContext(DashContext) as DashContextInterface;
+  const { crsData }: any = dash;
+  const items = crsData.rows;
+  useEffect(() => {
+    dashDispatch({ type: 'GETCRS', endpoint: 'cursos?limit=1000' });
+  }, [])
+
+  if (dash.checking == true || items === undefined) {
+    return (<Loader />)
+  }
+
+  console.log(crsData);
   return (
     <div className="app__dashboard">
       {/* <AddNewFab title="Nuevo Curso" /> */}
@@ -9,24 +23,56 @@ const CliCourses = () => {
       <div className="app__dashboard-row">
         <div className="app__dashboard-item">
           <Widget
-            title="Crear cursos"
+            title="Cursos disponibles"
           >
-            <p>Aqui puedes visualizar y modificar los cursos</p>
+            <p>Aquí puedes explorar los cursos que tenemos disponibles</p>
           </Widget>
         </div>
       </div>
-      {/* <div className="app__dashboard-row"> */}
-      <div
-        className="app__dashboard-item"
-      >
-        <Widget title="12341234">
-          <span className="app__course-state app__course-state--active">Disponible</span>
-          <div className="app__course-container">
-            <p>Mínimo de personas: </p>
-          </div>
-        </Widget>
-      </div>
-      {/* </div> */}
+      {
+        items.map((item: any) => {
+          const {
+            cursoActivo,
+            cursoIniciado,
+            empleado,
+            fechaFin,
+            fechaFinDeMatricula,
+            fechaIni,
+            horasTotales,
+            id,
+            maxMatriculados,
+            minMatriculados,
+            nombreCurso
+          } = item;
+          return (
+            <div
+              className="app__dashboard-item"
+              key={item.id}
+            >
+              <Widget title={nombreCurso}>
+                {
+                  cursoActivo
+                    ? <span className="app__course-state app__course-state--active">Disponible</span>
+                    : <span className="app__course-state app__course-state--inactive">No disponible</span>
+                }
+                <div className="app__course-container">
+                  <p>Mínimo de personas: <span>{minMatriculados}</span></p>
+                  <p>Máximo de personas: <span>{maxMatriculados}</span></p>
+                  <p>Comienza: <span>{fechaIni.split('T')[0]}</span></p>
+                  <p>Termina: <span>{fechaFin.split('T')[0]}</span></p>
+                  <p>Horas totales: <span>{horasTotales} hrs</span></p>
+                </div>
+                <div className="app__courses-instructor">
+                  <p>Instructor: {empleado.nombre} {empleado.apellido}</p>
+                </div>
+                <button>
+                  Inscribirse
+                </button>
+              </Widget>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
